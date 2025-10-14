48c728d0a91cde691f03a5d9c9411ab09768b300
MOHAMED AYMANE ELACHIRI <satayman41@gmail.com>
2025-10-14 13:03:05 +0100

    adding mvc chaptre

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
+// routes/web.php
 <?php
 
-// routes/web.php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
+// Route test
 Route::get('/ping', fn() => 'pong');
 
+// Routes nommées avec contrôleur
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/a-propos', [PageController::class, 'about'])->name('about');
+
+// Mini-routes articles (mockées pour l’instant)
+Route::get('/articles', [PageController::class, 'articles'])->name('articles.index');
+Route::get('/articles/{slug}', [PageController::class, 'show'])->name('articles.show');
